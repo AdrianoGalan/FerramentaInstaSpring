7d8092baaba258a05ab7c8c6fdad211c31b4fe43
AdrianoGalan <dricogalan@gmail.com>
2022-02-21 03:12:06 -0300

    serviso detalhe ok

---
+++ instaaaa.session.sql
@@ -0,0 +1,1 @@
+delete from perfil WHERE id = 36
--- src/main/java/com/adriano/controller/PerfilController.java
+++ src/main/java/com/adriano/controller/PerfilController.java
@@ -1,14 +1,16 @@
 package com.adriano.controller;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 import javax.validation.Valid;
 
-import com.adriano.model.Email;
 import com.adriano.model.Perfil;
 import com.adriano.model.Status;
-import com.adriano.repositotory.EmailRepository;
+
 import com.adriano.repositotory.PerfilRepository;
+import com.adriano.repositotory.StatusRepository;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,6 +19,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.AllArgsConstructor;
@@ -27,6 +30,7 @@ import lombok.AllArgsConstructor;
 public class PerfilController {
 
     private final PerfilRepository rPerfil;
+    private final StatusRepository rStatus;
 
     @PostMapping
     public ResponseEntity<String> insertPerfil(@Valid @RequestBody Perfil perfil) {
@@ -39,6 +43,33 @@ public class PerfilController {
 
     @PutMapping("/atualizar")
     public ResponseEntity<String> atualizarPerfil(@Valid @RequestBody Perfil perfil) {
+
+        this.rPerfil.save(perfil);
+
+        return ResponseEntity.ok("ok");
+
+    }
+
+    @PutMapping("/atualizar/status")
+    public ResponseEntity<String> cadastrarPerfil(@Valid @RequestBody Perfil perfil) {
+
+        Status status = rStatus.getByStatus(perfil.getStatus().getStatus());
+
+        switch (status.getStatus()) {
+            case "Cadastrado":
+                perfil.setDataCadastro(this.dataAtual());
+                break;
+            case "Trabalhando novo":
+                perfil.setDataInicioTrabalho(this.dataAtual());
+                break;
+            case "Bloqueado":
+                perfil.setDataBloqueio(this.dataAtual());
+                break;
+            default:
+                break;
+        }
+
+        perfil.setStatus(status);
 
         this.rPerfil.save(perfil);
 
@@ -67,6 +98,14 @@ public class PerfilController {
     public List<Perfil> listDifBloqueado(@PathVariable(value = "idStatus") int idStatus) {
 
         return this.rPerfil.findByStatusDIfBlo(idStatus);
+    }
+
+    private String dataAtual() {
+
+        Date data = new Date();
+        SimpleDateFormat formatador = new SimpleDateFormat("yyyy-MM-dd");
+        return formatador.format(data);
+
     }
 
 }
